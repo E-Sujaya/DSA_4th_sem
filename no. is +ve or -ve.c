1c04d003843331f068e3ac9438fcb133d51a0191
E-Sujaya <130632151+E-Sujaya@users.noreply.github.com>
2024-03-14 14:42:05 +0530

    Add files via upload

---
+++ no. is +ve or -ve.c
@@ -0,0 +1,12 @@
+//Program to check whether a number is positive or negative
+#include<stdio.h>
+main()
+{
+    int a;
+    printf("Enter an integer\n");
+    scanf("%d", &a);
+    if(a>0)
+        printf("It's a positive no.\n");
+    else
+        printf("It's a negative no.");
+}
